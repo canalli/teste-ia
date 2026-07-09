38c1eed1638ffcc3cba44641f4d5c1a0906bfb53
Rafael Canalli <rafael@canalli.net.br>
2026-07-09 17:22:18 -0300

    Update ping.py

--- ping.py
+++ ping.py
@@ -3,23 +3,23 @@ import subprocess
 def ping_host(ip_address: str) -> str:
     """
     Executa um ping em um endereço IP fornecido para verificar se o host está ativo.
-    
-    Args:
+     
+ Args:
         ip_address (str): O endereço IP a ser testado (ex: "8.8.8.8").
         
     Returns:
-        str: O resultado do comando ping.
-    """
+         str: O resultado do comando ping.
+     """
     
-    # Sanitizando a entrada para prevenir SQL Injection
+   # Sanitizando a entrada para prevenir SQL Injection
     sanitized_ip = subprocess.quote(ip_address)
-    
+   
     command = f"ping -c 1 {sanitized_ip}"
-    
-    try:
--        output = subprocess.run(command, shell=False, text=True)
-+        sanitized_command = subprocess.run(command, shell=False, text=True)
-         return sanitized_command.stdout if sanitized_command.returncode == 0 else "Falha ao executar o ping"
      
-    except subprocess.CalledProcessError as e:
-        return f"Falha ao executar o ping. Erro: {e}"
+     try:
+       output = subprocess.run(command, shell=False, text=True)
+       sanitized_command = subprocess.run(command, shell=False, text=True)
+       return sanitized_command.stdout if sanitized_command.returncode == 0 else "Falha ao executar o ping"
+     
+     except subprocess.CalledProcessError as e:
+         return f"Falha ao executar o ping. Erro: {e}"
